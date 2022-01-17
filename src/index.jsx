19ffdb07899a5e18577c7df0362986791128469e
laurentvac <laurentvac80@gmail.com>
2022-01-17 15:16:35 +0100

    customerlist

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CustomersListView from './screens/Customers/CustomersListView';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CustomersListView />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/screens/Customers/CustomersListView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CustomersListComponent from '../../components/Customers/CustomersList';
+
+const CustomersListView = () => {
+    return (
+        <div>
+           
+            <CustomersListComponent/>
+           
+        </div>
+    );
+};
+
+export default CustomersListView;
