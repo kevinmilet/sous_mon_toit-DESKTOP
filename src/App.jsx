7e8cb50cc1769cbabb106397bc0ac30c3a517a4f
Kevin Milet <kevin.milet@gmail.com>
2022-04-29 13:58:38 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,143 +1,157 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Context } from "./utils/context/Context";
 import CustomersListView from "./screens/Customers/CustomersListView";
 import CustomersDetailView from "./screens/Customers/CustomerDetailView";
-import axios from 'axios';
+import axios from "axios";
 import ApiRoutes from "./utils/const/ApiRoutes";
-import SnackbarProvider from 'react-simple-snackbar'
-import SignInView from './screens/SignIn/SignInView';
-import EstatesListView from './screens/Estates/EstatesListView';
-import DetailEstateView from './screens/Estates/DetailEstateView';
+import SnackbarProvider from "react-simple-snackbar";
+import SignInView from "./screens/SignIn/SignInView";
+import EstatesListView from "./screens/Estates/EstatesListView";
+import DetailEstateView from "./screens/Estates/DetailEstateView";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
 import HomeView from "./screens/Home/HomeView";
-import AddEstateView from './screens/Estates/AddEstateView';
+import AddEstateView from "./screens/Estates/AddEstateView";
 import StaffListView from "./screens/Staff/StaffListView";
-import AddEstateStep2View from './screens/Estates/AddEstateStep2View'
-import AddEstateStep3View from './screens/Estates/AddEstateStep3View'
+import AddEstateStep2View from "./screens/Estates/AddEstateStep2View";
+import AddEstateStep3View from "./screens/Estates/AddEstateStep3View";
 import StaffDetailsView from "./screens/Staff/StaffDetailsView";
 import AccountView from "./screens/AccountView/AccountView";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-    const [apiUrl, setApiUrl] = useState(ApiRoutes.API_URL);
-    const [token, setToken] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [showToast, setShowToast] = useState(false);
+  const [apiUrl, setApiUrl] = useState(ApiRoutes.API_URL);
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [showToast, setShowToast] = useState(false);
 
-    axios.defaults.headers.common = {
-
-        Authorization: `Bearer ${localStorage["token"]}`,
-    };
-    useEffect(() => {
-        // Test de la validité du token
-        axios.interceptors.response.use(function (response) {
-            return response
-        }, function (error) {
-            if (error.response) {
-                if (error.response.status === 401) {
-                    if (error.response.data.message === 'Wrong login or password') {
-                        return error.response;
-                    }
-                    localStorage.clear()
-                    return window.location = '/' // redirect to login page
-                }
+  axios.defaults.headers.common = {
+    Authorization: `Bearer ${localStorage["token"]}`,
+  };
+  useEffect(() => {
+    // Test de la validité du token
+    axios.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        if (error.response) {
+          if (error.response.status === 401) {
+            if (error.response.data.message === "Wrong login or password") {
+              return error.response;
             }
-        })
-        const storedToken = localStorage.getItem('token');
-        if (localStorage["token"]) {
-            setToken(storedToken);
-            console.log("il y a un token")
-        } else {
-            console.log("pas de token")
+            localStorage.clear();
+            return (window.location = "/"); // redirect to login page
+          }
         }
-        setLoading(false);
-
-    }, []);
-
-    if (loading) {
-        return <></>;
-    }
-
-    // function Notify() {
-    //     toast.info("Photo de profil modifiée",{
-    //         position: "top-right",
-    //         autoClose: 5000,
-    //         toastId: '1'
-    //     });
-    // }
-
-    return (
-        <Context.Provider value={{ apiUrl, setApiUrl, showToast, setShowToast }}>
-            <SnackbarProvider>
-                <div className="App">
-                    {/*<ToastContainer />*/}
-                    <div className="container-fluid m-0">
-                        <div className="row gx-0">
-                            <div className="col-md-2 m-0">
-                                <Sidebar />
-                            </div>
-                            <div className="col-md-10 m-0 d-flex flex-column">
-                                <div className="row-fluid">
-                                    <Topbar />
-                                </div>
-                                <div className="row-fluid">
-                                    {/*Content here.....*/}
-                                    <Router>
-                                        {token === null ? (
-                                            <React.Fragment>
-                                                <Route exact path="/">
-                                                    <SignInView />
-                                                </Route>
-                                            </React.Fragment>
-                                        ) : (
-                                            <React.Fragment>
-                                                <Route exact path="/">
-                                                    <HomeView />
-                                                </Route>
-                                            </React.Fragment>
-                                        )}
-                                        <Route exact path="/liste-des-biens">
-                                            <EstatesListView />
-                                        </Route>
-                                        <Route exact path="/detail-biens/:id">
-                                            <DetailEstateView />
-                                        </Route>
-                                        <Route exact path="/ajout-bien">
-                                            <AddEstateView />
-                                        </Route>
-                                        <Route exact path="/ajout-bien/step-2/:id">
-                                            <AddEstateStep2View />
-                                        </Route>
-                                        <Route exact path="/ajout-bien/step-3/:id">
-                                            <AddEstateStep3View />
-                                        </Route>
-                                        <Route exact path="/customers_list">
-                                            <CustomersListView />
-                                        </Route>
-                                        <Route exact path="/customer_detail/:id">
-                                            <CustomersDetailView />
-                                        </Route>
-                                        <Route exact path="/staff">
-                                            <StaffListView />
-                                        </Route>
-                                        <Route exact path="/details-staff/:id">
-                                            <StaffDetailsView />
-                                        </Route>
-                                        <Route exact path="/compte/:id">
-                                            <AccountView />
-                                        </Route>
-                                    </Router>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </SnackbarProvider>
-        </Context.Provider>
+      }
     );
-}
+    const storedToken = localStorage.getItem("token");
+    if (localStorage["token"]) {
+      setToken(storedToken);
+      console.log("il y a un token");
+    } else {
+      console.log("pas de token");
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <></>;
+  }
+
+  // function Notify() {
+  //     toast.info("Photo de profil modifiée",{
+  //         position: "top-right",
+  //         autoClose: 5000,
+  //         toastId: '1'
+  //     });
+  // }
+
+  return (
+    <Context.Provider value={{ apiUrl, setApiUrl, showToast, setShowToast }}>
+      <SnackbarProvider>
+        <div className="App">
+          {/*<ToastContainer />*/}
+          <div className="container-fluid m-0">
+            <div className="row gx-0">
+              <div className="col-md-2 m-0">
+                <Sidebar />
+              </div>
+              <div className="col-md-10 m-0 d-flex flex-column">
+                <div className="row-fluid">
+                  <Topbar />
+                </div>
+                <div className="row-fluid">
+                  {/*Content here.....*/}
+                  <HashRouter basename={"/"}>
+                    <Routes>
+                      {token === null ? (
+                        <React.Fragment>
+                          <Route exact path="/" element={<SignInView />} />
+                        </React.Fragment>
+                      ) : (
+                        <React.Fragment>
+                          <Route exact path="/" element={<HomeView />} />
+                        </React.Fragment>
+                      )}
+                      <Route
+                        exact
+                        path="/liste-des-biens"
+                        element={<EstatesListView />}
+                      />
+                      <Route
+                        exact
+                        path="/detail-biens/:id"
+                        element={<DetailEstateView />}
+                      />
+                      <Route
+                        exact
+                        path="/ajout-bien"
+                        element={<AddEstateView />}
+                      />
+                      <Route
+                        exact
+                        path="/ajout-bien/step-2/:id"
+                        element={<AddEstateStep2View />}
+                      />
+                      <Route
+                        exact
+                        path="/ajout-bien/step-3/:id"
+                        element={<AddEstateStep3View />}
+                      />
+                      <Route
+                        exact
+                        path="/customers_list"
+                        element={<CustomersListView />}
+                      />
+                      <Route
+                        exact
+                        path="/customer_detail/:id"
+                        element={<CustomersDetailView />}
+                      />
+                      <Route exact path="/staff" element={<StaffListView />} />
+                      <Route
+                        exact
+                        path="/details-staff/:id"
+                        element={<StaffDetailsView />}
+                      />
+                      <Route
+                        exact
+                        path="/compte/:id"
+                        element={<AccountView />}
+                      />
+                    </Routes>
+                  </HashRouter>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SnackbarProvider>
+    </Context.Provider>
+  );
+};
 
 export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,76 +1,92 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import colors from "../../utils/styles/colors";
-import logo from '../../assets/img/Logo_SousMonToit_square.png';
-import agendaIcon from '../../assets/icons/calendar.png';
-import estateIcon from '../../assets/icons/home_pink.png';
-import customerIcon from '../../assets/icons/value.png';
-import staffIcon from '../../assets/icons/seller.png';
-import rightArrow from '../../assets/icons/right-arrow.png';
+import logo from "../../assets/img/Logo_SousMonToit_square.png";
+import agendaIcon from "../../assets/icons/calendar.png";
+import estateIcon from "../../assets/icons/home_pink.png";
+import customerIcon from "../../assets/icons/value.png";
+import staffIcon from "../../assets/icons/seller.png";
+import rightArrow from "../../assets/icons/right-arrow.png";
 
 const MainContainer = styled.div`
-    width: 250px;
-    height: 100vh;
-    background-color: ${colors.backgroundPrimary};
-    -webkit-box-shadow: 0px 5px 6px rgba(0, 0, 0, 0.16); 
-    box-shadow: 0px 5px 6px rgba(0, 0, 0, 0.16);
-`
+  width: 250px;
+  height: 100vh;
+  background-color: ${colors.backgroundPrimary};
+  -webkit-box-shadow: 0px 5px 6px rgba(0, 0, 0, 0.16);
+  box-shadow: 0px 5px 6px rgba(0, 0, 0, 0.16);
+`;
 
 const Logo = styled.img`
-    width: 200px;
-    height: 200px;
-    margin: 20px;
-`
+  width: 200px;
+  height: 200px;
+  margin: 20px;
+`;
 
 const ButtonsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 200px;
-    margin: auto 20px;
-`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  margin: auto 20px;
+`;
 
 const Button = styled.div`
-    border: 1px solid ${colors.primaryBtn};
-    // border: none;
+  border: 1px solid ${colors.primaryBtn};
+  // border: none;
+  background-color: ${colors.backgroundPrimary};
+  border-radius: 50px;
+  color: ${colors.primary};
+  font-size: 18px;
+  width: 200px;
+  height: 50px;
+  margin: 10px auto;
+  &:hover {
+    border: 2px solid ${colors.secondaryBtn};
     background-color: ${colors.backgroundPrimary};
-    border-radius: 50px;
     color: ${colors.primary};
-    font-size: 18px;
-    width: 200px;
-    height: 50px;
-    margin: 10px auto;
-    &:hover {
-        border: 2px solid ${colors.secondaryBtn};
-        background-color: ${colors.backgroundPrimary};
-        color: ${colors.primary};
-    }
-`
+  }
+`;
 
 const Sidebar = () => {
-    return (
-        <div>
-            <MainContainer>
-                <Logo src={logo} className="logo" alt="Logo Sous mon toit"/>
-                <ButtonsContainer>
-                    <a href="/">
-                        <Button className="btn btn-secondary" ><img src={agendaIcon} width="35" height="35" alt=""/>&nbsp;Agenda&nbsp;<img src={rightArrow} width="18" height="18" alt=""/></Button>
-                    </a>
-                    <a href="/liste-des-biens">
-                        <Button className="btn btn-secondary"><img src={estateIcon} width="35" height="35" alt=""/>&nbsp;Biens&nbsp;<img src={rightArrow} width="18" height="18" alt=""/></Button>
-                    </a>
-                    <a href="/customers_list">
-                        <Button className="btn btn-secondary"><img src={customerIcon} width="35" height="35" alt=""/>&nbsp;Client&nbsp;<img src={rightArrow} width="18" height="18" alt=""/></Button>
-                    </a>
-                    <a href="/staff">
-                        <Button className="btn btn-secondary"><img src={staffIcon} width="35" height="35" alt=""/>&nbsp;Agents&nbsp;<img src={rightArrow} width="18" height="18" alt=""/></Button>
-                    </a>
-                    {/*<a href="#">*/}
-                    {/*    <Button className="btn btn-secondary"><img src={contractIcon} width="35" height="35" alt=""/>&nbsp;Contrat&nbsp;<img src={rightArrow} width="18" height="18" alt=""/></Button>*/}
-                    {/*</a>*/}
-                </ButtonsContainer>
-            </MainContainer>
-        </div>
-    );
+  return (
+    <div>
+      <MainContainer>
+        <Logo src={logo} className="logo" alt="Logo Sous mon toit" />
+        <ButtonsContainer>
+          <a href="/">
+            <Button className="btn btn-secondary">
+              <img src={agendaIcon} width="35" height="35" alt="" />
+              &nbsp;Agenda&nbsp;
+              <img src={rightArrow} width="18" height="18" alt="" />
+            </Button>
+          </a>
+          <a href="/liste-des-biens">
+            <Button className="btn btn-secondary">
+              <img src={estateIcon} width="35" height="35" alt="" />
+              &nbsp;Biens&nbsp;
+              <img src={rightArrow} width="18" height="18" alt="" />
+            </Button>
+          </a>
+          <a href="/customers_list">
+            <Button className="btn btn-secondary">
+              <img src={customerIcon} width="35" height="35" alt="" />
+              &nbsp;Client&nbsp;
+              <img src={rightArrow} width="18" height="18" alt="" />
+            </Button>
+          </a>
+          <a href="/staff">
+            <Button className="btn btn-secondary">
+              <img src={staffIcon} width="35" height="35" alt="" />
+              &nbsp;Agents&nbsp;
+              <img src={rightArrow} width="18" height="18" alt="" />
+            </Button>
+          </a>
+          {/*<a href="#">*/}
+          {/*    <Button className="btn btn-secondary"><img src={contractIcon} width="35" height="35" alt=""/>&nbsp;Contrat&nbsp;<img src={rightArrow} width="18" height="18" alt=""/></Button>*/}
+          {/*</a>*/}
+        </ButtonsContainer>
+      </MainContainer>
+    </div>
+  );
 };
 
 export default Sidebar;
